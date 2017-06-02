cd265cd940f6c56bad4cdac22b70b701c88aadbc
Sam Heller <sheller0@gmail.com>
2017-06-02 00:48:14 -0700

    Ferreal Initial Commit
    Initial configuration files

---
+++ data/get-data.sh
@@ -0,0 +1,5 @@
+#SPD 911 report data
+wget -O 911-data.csv https://data.seattle.gov/api/views/3k2p-39jp/rows.csv?accessType=DOWNLOAD
+
+#Seattle Neighborhood GeoBoundary data
+wget https://raw.githubusercontent.com/seattleio/seattle-boundaries-data/master/data/neighborhoods.geojson
